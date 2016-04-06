94caa80cbee2828ed0801465b98625b47000eb94
nickchoi <nicholas_choi@miacanalytics.com>
2016-04-06 12:01:32 -0400

    Swtiched to Discord.Net instead of DiscordSharp

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Net;
+using Discord.Legacy;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new DiscordClient();
+
+            //Display all log messages in the console
+            client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}: {e.Message}");
+
+            //Echo back any message received, provided it didn't come from the bot itself
+            client.MessageReceived += async (s, e) =>
+            {
+                if (!e.Message.IsAuthor)
+                    await e.Channel.SendMessage(e.Message.Text);
+            };
+
+            //Convert our sync method to an async one and block the Main function until the bot disconnects
+            client.ExecuteAndWait(async () =>
+            {
+                //Connect to the Discord server using our email and password
+                await client.Connect("enchoibots@gmail.com", "nickchoi1");
+
+
+            });
+        }
+    }
+}
